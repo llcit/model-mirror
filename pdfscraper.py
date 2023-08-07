e5f2f1f2d7d6307e590b17443fc394ea40927efb
zeb1283 <70921412+zeb1283@users.noreply.github.com>
2023-08-07 10:43:35 -1000

    essential update pdfscraper.py

--- pdfscraper.py
+++ pdfscraper.py
@@ -2,7 +2,7 @@ import fitz
 import openai
 import json
 
-APIKEY = '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'
+APIKEY = 'examplekey'
 openai.api_key = APIKEY
 
 #select the language you want
@@ -17,16 +17,17 @@ else:
     #reads pdf page uploads in sequence
     c = ''
     for page in doc.pages(2,doc.page_count,1):
-        if(len(c) < 60000):
+        if(len(c) < 40000):
             extract = page.get_text()
             c += extract
 doc.close()
 
 #changes number of passages based on text length
 num = int(len(c)/10000) + 3
+print(num)
 numPassages = str(num)
-text = 'QUERY: write ' + numPassages + ' 120-word passages in' + language + ' about this exerpt: ' + c
-initInstruction = 'You are a language teacher tasked with developing 120 word passages for the purpose of language instruction. These passages will be used to test language learners with their corresponding literacy levels. Generate unique passages as a base for this learning process.'
+text = 'QUERY: write ' + numPassages + ' extremely unique 120-word passages with differing word choice in' + language + ' about this exerpt: ' + c + '. \n\n Each passage should be about a different concept discussed in the excerpt.'
+initInstruction = 'You are a language teacher tasked with developing unique 120 word passages for the purpose of language instruction. These passages will be used to test language learners with their corresponding literacy levels. Generate' + numPassages + ' unique passages as a base for this learning process.'
 
 #returns the base passages
 def returnPassages(sysinput, cinput):
@@ -40,7 +41,7 @@ def returnPassages(sysinput, cinput):
         {"role": "user", "content": cinput}],
         max_tokens=1000,
         #changes the randomness
-        temperature=0.6,   
+        temperature=0   
     )
     else:
         response = openai.ChatCompletion.create(
@@ -49,7 +50,7 @@ def returnPassages(sysinput, cinput):
         {"role": "system", "content": sysinput},
         {"role": "user", "content": cinput}],
         max_tokens=1000,
-        temperature=0.5,   
+        temperature=0   
     )
     reply = response['choices'][0]['message']['content']
     return reply
@@ -58,9 +59,9 @@ reply = returnPassages(initInstruction, text)
 
 #diff definitions of difficulty
 intro = 'You are a writer tasked with modifying a set of passages to a set of instructions. These are the instructions: '
-beginnerInstruction = 'Simplify the following passages to beginner level, which means the language used has short sentences, simple grammar patterns, and uses vocabulary words of high frequency. Separate these passages with #### as a delimiter'
-intermediateInstruction = 'Modify the following passages to intermediate level, which means that the language used has somewhat complex grammar, more infrequent vocabulary, and longer sentences, but does not contain extremely complex grammar, jargon, or complex sentences.'
-advancedInstruction = 'Modify the following passages to advanced level, which means that the language used has complex grammar, contains infrequent vocabulary words and jargon, and has lengthy sentences.'
+beginnerInstruction = 'Simplify the following passages to beginner level, which means the language used has short sentences, simple grammar patterns, and uses vocabulary words of high frequency. Always separate every passage that is generated with #### as a delimiter. Do not put a delimiter before the first passage.'
+intermediateInstruction = 'Modify the following passages to intermediate level, which means that the language used has somewhat complex grammar, more infrequent vocabulary, and longer sentences, but does not contain extremely complex grammar, jargon, or complex sentences. Always separate every passage that is generated with #### as a delimiter. Do not put a delimiter before the first passage.''
+advancedInstruction = 'Modify the following passages to advanced level, which means that the language used has complex grammar, contains infrequent vocabulary words and jargon, and has lengthy sentences. Always separate every passage that is generated with #### as a delimiter. Do not put a delimiter before the first passage.''
 
 userInput = intro + beginnerInstruction
 
@@ -79,13 +80,14 @@ def simplifyReplies(sysinput, cinput):
 finalPassages = simplifyReplies(userInput, reply)
 
 #if you want to store into file
-#with open('FULLRUNTEST.txt', 'a') as f:
-#    f.write('\n\nSYSTEM PROMPT: ' + initInstruction + '\n\n')
-#    f.write('QUERY: write 3 200-word passages in english about this exerpt: \n\n')
-#    f.write('TEXT INPUT: ' + text + '\n\n')
-#    f.write('RESPONSE: ' + reply + '\n\n')
-#    f.write('QUERY 2: ' + userInput + '\n\n')
-#    f.write('Final Passsages' + finalPassages)
+with open('FULLRUNTEST.txt', 'a') as f:
+    f.write('\n\nSYSTEM PROMPT: ' + initInstruction + '\n\n')
+    f.write('QUERY: write 3 200-word passages in english about this exerpt: \n\n')
+    f.write('TEXT INPUT: ' + text + '\n\n')
+    f.write('RESPONSE: ' + reply + '\n\n')
+    f.write('QUERY 2: ' + userInput + '\n\n')
+    f.write('Final Passsages' + finalPassages)
+
 #split into array indices
 arr = finalPassages.split("####")
 
